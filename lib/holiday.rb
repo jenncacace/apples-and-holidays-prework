23a1057602c5d59303ac3cb126facb693d0df01b
Jenn Cacace <jenn.cacace@gmail.com>
2018-01-18 15:17:34 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -23,8 +23,9 @@ end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
 
- holiday_hash[:winter].each do |holiday, supplies|
+  holiday_hash[:winter].each do |holiday, supplies|
    supplies.push(supply)
+  end 
  end 
 
 
